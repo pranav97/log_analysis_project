a45b500018a2ce0d89234dd4f317343a19f4f796
Pranav Raghavan <gtalkpranav@gmail.com>
2018-09-20 19:41:58 -0700

    ran pycodestyle

--- popular_articles.py
+++ popular_articles.py
@@ -4,17 +4,19 @@ import psycopg2
 
 def get_popular_articles():
     """
-    The logs table stores the visits themselves from the users. We need to count each distinct path and compile a list
-    of the most visited articles
+    The logs table stores the visits themselves from the users. We need to
+    count each distinct path and compile a list of the most visited articles
     :return: print the most popular articles
     """
     db = psycopg2.connect("dbname=news")
-    """ 
-    Using a string concatenation in sql using the || operator to join articles.slug and log.path we are able to figure
-    out the names of the articles's names
+    """
+    Using a string concatenation in sql using the || operator to join 
+    articles.slug and log.path we are able to figure out the names of the 
+    articles's names
     """
     cur = db.cursor()
-    # I found a website that makes sql commands that I type look professional like this one
+    # I found a website that makes sql commands that I type look professional
+    # like this one
     # http://www.dpriver.com/pp/sqlformat.htm
     cur.execute(
         """
@@ -39,8 +41,9 @@ def get_popular_articles():
 
 def get_popular_authors():
     """
-    We take the join from earlier and join it to another table, this one is called authors, this join is pretty straight
-    forward and can be done with the authors.id = articles.author
+    We take the join from earlier and join it to another table, this one is
+    called authors, this join is pretty straight forward and can be done with
+    the authors.id = articles.author
     :return:
     """
     db = psycopg2.connect("dbname=news")
@@ -77,31 +80,32 @@ def get_error_days():
 
     # inner most query gets the total
     # 2nd inner most query gets the error rate every single day
-    # last select is just filetering the error rate by the 1.00% threshold
+    # last select is just filtering the error rate by the 1.00% threshold
     cur.execute(
         """
-            SELECT To_char(day, 'Month DD, YYYY'),
-                Cast (failrate AS DECIMAL(10,2))
-            FROM   (SELECT totaltable.total,
-                           Count(status)
-                           AS
-                                  failed,
-                           Date(time)
-                           AS day,
-                           ( ( Cast (Count(status) AS DECIMAL) / totaltable.total ) * 100 )
-                           AS
-                                  failrate
-                    FROM   log
-                           JOIN (SELECT Count(Date(time)) AS total,
-                                        Date(time)        AS day
-                                 FROM   log
-                                 GROUP  BY Date(time)) AS totaltable
-                             ON totaltable.day = Date(time)
-                    WHERE  status = '404 NOT FOUND'
-                    GROUP  BY Date(time),
-                              status,
-                              totaltable.total) AS failtable
-            WHERE  failrate > 1.0;
+        SELECT To_char(day, 'Month DD, YYYY'),
+            Cast (failrate AS DECIMAL(10,2))
+        FROM   (SELECT totaltable.total,
+                       Count(status)
+                       AS
+                              failed,
+                       Date(time)
+                       AS day,
+                       ( ( Cast (Count(status)
+                        AS DECIMAL) / totaltable.total ) * 100 )
+                       AS
+                              failrate
+                FROM   log
+                       JOIN (SELECT Count(Date(time)) AS total,
+                                    Date(time)        AS day
+                             FROM   log
+                             GROUP  BY Date(time)) AS totaltable
+                         ON totaltable.day = Date(time)
+                WHERE  status = '404 NOT FOUND'
+                GROUP  BY Date(time),
+                          status,
+                          totaltable.total) AS failtable
+        WHERE  failrate > 1.0;
         """
     )
     rows = cur.fetchall()
